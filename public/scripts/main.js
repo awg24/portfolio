3e822f0baadb6d90c95e01e2400aa9144cf76ca7
allen wes <allen.wes.g@gmail.com>
2016-10-04 02:49:01 -0500

    got rid of some unused code and text changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,3 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
-
-
 });
-
-function removeError() {
-	var element = document.getElementById("error-message");
-	element.parentNode.removeChild(element)
-}
-
-function removeSuccess() {
-	var element = document.getElementById("success-message");
-	element.parentNode.removeChild(element)
-}
